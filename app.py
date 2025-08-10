5ddda3229f959f34350d27bc5b9d07b12339e068
unknown <mitrashabani93@gmail.com>
2025-08-10 17:33:35 -0400

    Init

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+from transformers import pipeline
+
+# Load model
+sentiment_analyzer = pipeline("sentiment-analysis")
+
+
+# Title & Layout
+st.title("Sentiment Analysis App")
+st.write("Enter text below to analyze its sentiment:")
+userInput = st.text_area("Your test: ")
+
+if st.button("Analyze Sentiment"):
+    # Ensure not empty
+    if userInput.strip():
+        result = sentiment_analyzer(userInput)[0]
+        label = result['label']
+        score = round(result['score'], 3)
+
+        st.write(f"Sentiment :  {label}")
+        st.write(f"Confidence :  {score}")
+    else:
+        st.warning("Your text area is empty.")
